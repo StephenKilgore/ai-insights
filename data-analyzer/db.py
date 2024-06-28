eac899a132718296ce302278d4bd30531d8bde68
stephen kilgore <stephenkilgore07@gmail.com>
2024-06-28 00:50:24 -0500

    fixing deploy.yml

--- data-analyzer/db.py
+++ data-analyzer/db.py
@@ -9,17 +9,8 @@ POSTGRES_USER = os.getenv('POSTGRES_USER')
 POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 POSTGRES_HOST = os.getenv('POSTGRES_HOST')
 POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-
 DATABASE_URI = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
-
-# Create the database engine
 engine = create_engine(DATABASE_URI)
-
-# Define a base class for declarative class definitions
 Base = declarative_base()
-
-# Create a session factory
 Session = sessionmaker(bind=engine)
-
-# Create a new session
 session = Session()

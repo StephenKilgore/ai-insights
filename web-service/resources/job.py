4165d542d10ae5fe7d2211b71c9e9ebb7a973036
stephen kilgore <stephenkilgore07@gmail.com>
2024-07-01 18:05:40 -0500

    added /job and /job/<id> endpoint. fixed class name in resources.message

--- web-service/resources/job.py
+++ web-service/resources/job.py
@@ -1,8 +1,20 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import db
-from models import job
+from models import JobModel
 from schemas import JobSchema
 from sqlalchemy.exc import SQLAlchemyError
 
 blp = Blueprint("jobs", __name__, description="Operations on jobs")
+
+@blp.route("/job")
+class JobList(MethodView):
+    @blp.response(200, JobSchema(many=True))
+    def get(self):
+        return JobModel.query.all()
+
+@blp.route("/job/<string:job_id>")
+class Job(MethodView):
+    @blp.response(200, JobSchema())
+    def get(self, job_id):
+        return JobModel.query.get_or_404(job_id)

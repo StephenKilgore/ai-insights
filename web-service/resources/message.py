a8eba5c976be447a5a046dd09b4c99a9aeec9b9c
stephen kilgore <stephenkilgore07@gmail.com>
2024-07-01 16:25:48 -0500

    added /message route

--- web-service/resources/message.py
+++ web-service/resources/message.py
@@ -1,8 +1,15 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import db
-from models import message
+from models import MessageModel
 from schemas import MessageSchema
 from sqlalchemy.exc import SQLAlchemyError
 
 blp = Blueprint("messages", __name__, description="Operations on messages")
+
+
+@blp.route("/message")
+class Message(MethodView):
+    @blp.response(200, MessageSchema(many=True))
+    def get(self):
+        return MessageModel.query.all()

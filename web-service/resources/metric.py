d86e404b058d62328329b03da162d6c96901bf3d
Stephen Kilgore <stephenkilgore07@gmail.com>
2024-10-13 13:52:34 -0500

    adding metrics endpoint

--- web-service/resources/metric.py
+++ web-service/resources/metric.py
@@ -12,12 +12,12 @@ blp = Blueprint("metrics", __name__, description="Operations on metrics")
 class Metric(MethodView):
     @blp.response(200, MetricSchema())
     def get(self):
-        total_messages = db.session.query(func.count(message.id)).scalar()
-        total_jobs = db.session.query(func.count(job.id)).scalar()
-        jobs_with_failed_rows = db.session.query(func.count(job.id)).filter(job.rows_failed > 0).scalar()
+        total_messages = db.session.query(func.count(MessageModel.id)).scalar()
+        total_jobs = db.session.query(func.count(JobModel.id)).scalar()
+        jobs_with_failed_rows = db.session.query(func.count(JobModel.id)).filter(JobModel.rows_failed > 0).scalar()
         avg_messages_per_job = total_messages / total_jobs
-        avg_job_runtime = db.session.query(func.avg(job.runtime)).scalar()
-        last_job_run_date = db.session.query(func.max(job.job_end_date)).scalar()
+        avg_job_runtime = db.session.query(func.avg(JobModel.runtime)).scalar()
+        last_job_run_date = db.session.query(func.max(JobModel.job_end_date)).scalar()
 
         return {
             "total_messages": total_messages,
